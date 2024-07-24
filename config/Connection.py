d01e47261f476e18613b1062870f9aebeaa462e9
AngelinaBassano <bassanoangelina2002@gmail.com>
2024-07-24 13:10:40 +0200

    structure

--- config/Connection.py
+++ config/Connection.py
@@ -1,11 +1,11 @@
+from dotenv import load_dotenv
 import psycopg2
+import os
 
-connection = psycopg2.connect(
-    host='127.0.0.1',
-    port='5432',
-    user='postgres',
-    password='1234ab',
-    database='Library_Manager'
-)
+
+load_dotenv()
+
+connection = psycopg2.connect(host=os.getenv('DB_HOST'), port=os.getenv('DB_PORT'), user=os.getenv('DB_USER'),
+                                              password=os.getenv('DB_PASSWORD'), database=os.getenv('DB_NAMEDB'))
 
 print(connection)
