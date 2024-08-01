989536ba11f0a298bbc71729ca6890f17441f265
pilimuino <pili.muino@gmail.com>
2024-08-01 21:53:47 +0200

    Create books tests MLM-46

--- test/test_books.py
+++ test/test_books.py
@@ -41,3 +41,63 @@ def test_verify_data_not_found(mocker):
         'status_code': 404,
         'response': 'Don’t verify data'
     }
+
+
+def test_verify_data_exception(mocker):
+    mock_book_model = mocker.patch('src.controllers.BookController.BookModel')
+    mock_book_model_instance = mock_book_model.return_value
+    mock_book_model_instance.verify_data.side_effect = Exception("Mocked exception")
+
+    book_controller = BookController()
+    book_controller.book_model = mock_book_model_instance
+    book_id = "Book_id Test"
+    title = "Title Test"
+    author = "Author Test"
+    category_id = "Category Test"
+
+    response = book_controller.verify_data(book_id, title, author, category_id)
+
+    assert response == {
+        'status_code': 500,
+        'response': 'Error verifying data: Mocked exception'
+    }
+
+
+def test_create_book_success(mocker):
+    mock_book_model = mocker.patch('src.controllers.BookController.BookModel')
+    mock_book_model_instance = mock_book_model.return_value
+    mock_book_model_instance.create_book.return_value = None
+    book_controller = BookController()
+    book_id = "Book_id Test"
+    title = "Title Test"
+    author = "Author Test"
+    category_id = "Category Test"
+    stock = 10
+
+    response = book_controller.create_book(book_id, title, author, category_id, stock)
+
+    expected_result = {
+        'result': f'{"Book_id Test", "Title Test", "Author Test", "Category Test", 10}'
+    }
+    assert response == expected_result
+    mock_book_model_instance.create_book.assert_called_once_with(book_id, title, author, category_id, stock)
+
+
+def test_create_book_failure(mocker):
+    mock_book_model = mocker.patch('src.controllers.BookController.BookModel')
+    mock_book_model_instance = mock_book_model.return_value
+    mock_book_model_instance.create_book.side_effect = Exception("Mocked exception")
+    book_controller = BookController()
+    book_id = "Book_id Test"
+    title = "Title Test"
+    author = "Author Test"
+    category_id = "Category Test"
+    stock = 10
+
+    response = book_controller.create_book(book_id, title, author, category_id, stock)
+
+    assert response == {
+        'status_code': 500,
+        'response': 'Error when creating the book: Mocked exception'
+    }
+    mock_book_model_instance.create_book.assert_called_once_with(book_id, title, author, category_id, stock)
