95a3c51fd2948c46f52c22487c8f5e6eb946c1d8
AngelinaBassano <bassanoangelina2002@gmail.com>
2024-08-13 13:27:29 +0200

    Create book Model Fixes MLM-34

--- models/BookModel.py
+++ models/BookModel.py
@@ -7,16 +7,16 @@ class BookModel:
         self.db = Connection()
 
     def verify_data(self, book_id, title, author, category_id):
-        query = "SELECT * FROM books WHERE (book_id = %S, title = %S, author = %s, category_id = %s)"
+        query = "SELECT * FROM books WHERE (book_id = %s, title = %s, author = %s, category_id = %s)"
         params = (book_id, title, author, category_id)
         try:
             return self.db.execute_query(query, params)
         except Exception as e:
             return f'Error verifying data: {e}'
 
-    def create_book(self, title, author, category_id, stock):
-        query = "INSERT INTO books(title, author, category_id, stock) VALUES(%s, %s, %s, %s)"
-        params = (title, author, category_id, stock)
+    def create_book(self, book_id, title, author, category_id, stock):
+        query = "INSERT INTO books(book_id, title, author, category_id, stock) VALUES(%s, %s, %s, %s, %s)"
+        params = (category_id, title, author, category_id, stock)
         try:
             return self.db.update_query(query, params)
         except Exception as e:
@@ -39,7 +39,7 @@ class BookModel:
             return f'Error finding the title of the Book: {e}'
 
     def get_book_by_author(self, author):
-        query = "SELECT * FROM books WHERE author = %s"
+        query = "SELECT * FROM books WHERE (author = %s)"
         params = (author,)
         try:
             return self.db.execute_query(query, params)
