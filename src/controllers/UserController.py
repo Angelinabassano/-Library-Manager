aac05a7a3e96469509107fb2bc3eb7e765888f6e
Angelica perez <angelicanakaridperezpino@gmail.com>
2024-08-16 01:34:55 +0200

    verify user and create user data UserController MLM-10

--- src/controllers/UserController.py
+++ src/controllers/UserController.py
@@ -1,1 +1,23 @@
-º
+from models.UserModel import UsersModels
+
+
+class UserController:
+
+    def __init__(self):
+        self.user_model = UsersModels()
+
+    def create_user(self, first_name, last_name, email, phone_number, address):
+        try:
+            if self.user_model.verify_user(email):
+                return dict(status_code=400,
+                            response= 'El usuario ya existe',
+                            result=[first_name, last_name, email, phone_number, address])
+            self.user_model.create_user(first_name, last_name, email, phone_number, address)
+            return dict(status_code=200,
+                        response='El usuario se creo de manera exitosa',
+                        result=[first_name, last_name, email, phone_number, address])
+        except Exception as e:
+            return dict(status_code=500,
+                        response=f'Error al crear el usuario: {e}')
+
+
