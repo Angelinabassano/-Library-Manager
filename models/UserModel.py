6bb0a8f62f62f2c8ddb749c425b276043448dc04
Angelica perez <angelicanakaridperezpino@gmail.com>
2024-08-18 16:09:00 +0200

    update and edit user and UserController MLM-11

--- models/UserModel.py
+++ models/UserModel.py
@@ -5,17 +5,16 @@ class UsersModels:
 
     def __init__(self):
         self.db = Connection()
+
     def verify_user(self, email):
         try:
             query = "SELECT * FROM users WHERE email = %s"
             params = (email,)
             result = self.db.execute_query(query, params)
             return result
-            print(result)
         except Exception as e:
             print(f"Error verifying user: {e}")
             return None
-
 
     def create_user(self, first_name, last_name, email, phone_number, address):  # aqui falta un try cath
         try:
@@ -26,11 +25,9 @@ class UsersModels:
             print(f"Error creating user: {e}")
             return None
 
-
-
     def get_user(self, user_id):
         try:
-            query = "SELECT * FROM users WHERE id = %s"
+            query = "SELECT * FROM users WHERE user_id = %s"
             params = (user_id,)
             result = self.db.execute_query(query, params)
             return result
@@ -38,40 +35,39 @@ class UsersModels:
             print(f"Error getting user: {e}")
             return None
 
-    def edit_user(self, user_id, first_name=None, last_name=None, phone_number=None, address=None):
+    def edit_user(self, user_id, first_name, last_name, phone_number, address):
         try:
             updates = []
             params = []
 
             if first_name:
                 updates.append("first_name = %s")
-                params.append(first_name)
+                params.append(first_name,)
             if last_name:
                 updates.append("last_name = %s")
-                params.append(last_name)
+                params.append(last_name,)
             if phone_number:
                 updates.append("phone_number = %s")
-                params.append(phone_number)
+                params.append(phone_number,)
             if address:
                 updates.append("address = %s")
-                params.append(address)
+                params.append(address,)
 
             if not updates:
-                return None
+                return True
 
-            query = f"UPDATE users SET {', '.join(updates)} WHERE id = %s"
+            query = f"UPDATE users SET {', '.join(updates)} WHERE user_id = %s"
             params.append(user_id)
 
-            return self.db.update_query(query, tuple(params))
+            return self.db.update_query(query, params)
 
         except Exception as e:
             print(f"Error editing user: {e}")
-            return None
-
+            return False
 
     def delete_user(self, user_id):
         try:
-            query = "DELETE FROM users WHERE id = %s"
+            query = "DELETE FROM users WHERE user_id = %s"
             params = (user_id,)
             return self.db.update_query(query, params)
         except Exception as e:
