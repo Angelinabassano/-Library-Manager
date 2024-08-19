68606a99d8567baeee3ecdbed993fe0609e6bd06
AngelinaBassano <bassanoangelina2002@gmail.com>
2024-08-19 22:09:55 +0200

    Create Category Test MLM-113

--- test/test_category.py
+++ test/test_category.py
@@ -55,3 +55,40 @@ def test_verify_data_exception(mocker):
         'status_code': 500,
         'response': f'Error verifying category: Mocked exception'
     }
+
+
+def test_create_category_success(mocker):
+    mock_category_model = mocker.patch('src.controllers.CategoryController.CategoryModel')
+    mock_category_model_instance = mock_category_model.return_value
+    mock_category_model_instance.create_category.return_value = None
+
+    category_controller = CategoryController()
+    category_id = "Category_id"
+    category_name = "Category Test"
+
+    response = category_controller.create_category(category_id, category_name)
+
+    expected_result = {
+        'result': f'{'Category_id', 'Category Test'}'
+    }
+    assert response == expected_result
+    mock_category_model_instance.create_category.assert_called_once_with(category_id, category_name)
+
+
+def test_create_category_failed(mocker):
+    mock_category_model = mocker.patch('src.controllers.CategoryController.CategoryModel')
+    mock_category_model_instance = mock_category_model.return_value
+    mock_category_model_instance.create_category.side_effect = Exception("Mocked exception")
+
+    category_controller = CategoryController()
+    category_id = "Category_id"
+    category_name = "Category Test"
+
+    response = category_controller.create_category(category_id, category_name)
+
+    assert response == {
+        'status_code': 500,
+        'response': f'Error when creating the category: Mocked exception'
+    }
+    mock_category_model_instance.create_category.assert_called_once_with(category_id, category_name)
+
